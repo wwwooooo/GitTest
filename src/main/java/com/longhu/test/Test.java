efa3d4b6dc76e5614e959f9957c6e4b6c03e6a33
睡不醒的程序员 <2206463692@qq.com>
2022-06-05 16:04:11 +0800

    第一次提交

---
+++ src/main/java/com/longhu/test/Test.java
@@ -0,0 +1,11 @@
+package com.longhu.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("2131212332");
+        System.out.println("2131212332");
+        System.out.println("2131212332");
+        System.out.println("2131212332");
+
+    }
+}
